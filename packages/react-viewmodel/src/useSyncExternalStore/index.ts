6b2d7dab91a62394179511830083c9b38a34bc4f
guanghechen <me.guanghechen@gmail.com>
2023-07-29 19:36:56 +0800

    :bug:  fix(react-viewmodel): export error

--- packages/react-viewmodel/src/useSyncExternalStore/index.ts
+++ packages/react-viewmodel/src/useSyncExternalStore/index.ts
@@ -1,7 +1,7 @@
 /**
  * @see https://github.com/facebook/react/blob/493f72b0a7111b601c16b8ad8bc2649d82c184a0/packages/use-sync-external-store/src/useSyncExternalStoreShim.js
  */
-import { useSyncExternalStore as builtin } from 'react'
+import React from 'react'
 import { useSyncExternalStore as client } from './client'
 import { useSyncExternalStore as server } from './server'
 
@@ -12,6 +12,7 @@ const canUseDOM: boolean = !!(
 )
 
 const shim = canUseDOM ? client : server
+const builtin = React.useSyncExternalStore
 
 export const useSyncExternalStore: <T>(
   subscribe: (onStoreChange: () => void) => () => void,
