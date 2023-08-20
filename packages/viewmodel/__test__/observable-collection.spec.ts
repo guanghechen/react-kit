284195da86ba8bc06f8ff6456575307a7588249d
guanghechen <me.guanghechen@gmail.com>
2023-08-20 19:26:33 +0800

    :white_check_mark:  test(viewmodel): update tests for observable-collection

--- packages/viewmodel/__test__/observable-collection.spec.ts
+++ packages/viewmodel/__test__/observable-collection.spec.ts
@@ -2,7 +2,7 @@ import type { IConsoleMock } from '@guanghechen/helper-jest'
 import { createConsoleMock } from '@guanghechen/helper-jest'
 import { ObservableCollection } from '../src'
 import type { IImmutableCollection, IObservableCollection } from '../src'
-import { ImmutableMap, Subscriber, delay } from './_common'
+import { ImmutableMap, Subscriber } from './_common'
 
 describe('ObservableMap', () => {
   let observableMap: IObservableCollection<string, string, IImmutableCollection<string, string>>
@@ -27,41 +27,88 @@ describe('ObservableMap', () => {
   test('observeKey', async () => {
     const observableA = observableMap.observeKey('A')
     const observableB = observableMap.observeKey('B')
+    const observableC = observableMap.observeKey('C')
     const subscriberA = new Subscriber<string>('A', '')
     const subscriberB = new Subscriber<string>('B', '')
+    const subscriberC = new Subscriber<string>('C', '')
     const unsubscribableA = observableA.subscribe(subscriberA)
-    const unsubscribableB = observableA.subscribe(subscriberA)
+    const unsubscribableB = observableB.subscribe(subscriberB)
+    const unsubscribableC = observableC.subscribe(subscriberC)
 
     expect(observableA.disposed).toEqual(false)
     expect(observableB.disposed).toEqual(false)
 
     expect(observableA.getSnapshot()).toEqual(undefined)
     expect(observableB.getSnapshot()).toEqual(undefined)
+    expect(observableC.getSnapshot()).toEqual(undefined)
     expect(subscriberA.value).toEqual('')
     expect(subscriberB.value).toEqual('')
+    expect(subscriberC.value).toEqual('')
 
-    observableMap.set('A', 'waw')
-    expect(observableA.getSnapshot()).toEqual(undefined)
+    observableMap.set('A', 'waw1')
+    expect(observableA.getSnapshot()).toEqual('waw1')
     expect(observableB.getSnapshot()).toEqual(undefined)
-    expect(subscriberA.value).toEqual('')
+    expect(observableC.getSnapshot()).toEqual(undefined)
+    expect(subscriberA.value).toEqual('waw1')
     expect(subscriberB.value).toEqual('')
-    await delay(100)
-    expect(observableA.getSnapshot()).toEqual('waw')
-    expect(observableB.getSnapshot()).toEqual(undefined)
-    expect(subscriberA.value).toEqual('waw')
-    expect(subscriberB.value).toEqual('')
+    expect(subscriberC.value).toEqual('')
 
     observableMap.set('B', 'waw2')
-    expect(observableA.getSnapshot()).toEqual('waw')
-    expect(observableB.getSnapshot()).toEqual(undefined)
-    expect(subscriberA.value).toEqual('waw')
-    expect(subscriberB.value).toEqual('')
-    await delay(100)
-    expect(observableA.getSnapshot()).toEqual('waw')
+    expect(observableA.getSnapshot()).toEqual('waw1')
     expect(observableB.getSnapshot()).toEqual('waw2')
-    expect(subscriberA.value).toEqual('waw')
+    expect(observableC.getSnapshot()).toEqual(undefined)
+    expect(subscriberA.value).toEqual('waw1')
     expect(subscriberB.value).toEqual('waw2')
+    expect(subscriberC.value).toEqual('')
 
-    expect(consoleMock.getIndiscriminateAll()).toMatchInlineSnapshot()
+    observableMap.set('C', 'waw3')
+    observableMap.delete('B')
+    expect(observableA.getSnapshot()).toEqual('waw1')
+    expect(observableB.getSnapshot()).toEqual(undefined)
+    expect(observableC.getSnapshot()).toEqual('waw3')
+    expect(subscriberA.value).toEqual('waw1')
+    expect(subscriberB.value).toEqual(undefined)
+    expect(subscriberC.value).toEqual('waw3')
+
+    observableMap.merge([
+      ['A', 'waw1_2'],
+      ['B', 'waw2_2'],
+    ])
+    expect(observableA.getSnapshot()).toEqual('waw1_2')
+    expect(observableB.getSnapshot()).toEqual('waw2_2')
+    expect(observableC.getSnapshot()).toEqual('waw3')
+    expect(subscriberA.value).toEqual('waw1_2')
+    expect(subscriberB.value).toEqual('waw2_2')
+    expect(subscriberC.value).toEqual('waw3')
+
+    unsubscribableB.unsubscribe()
+
+    observableMap.merge([
+      ['B', 'waw2_3'],
+      ['C', 'waw3_3'],
+    ])
+    expect(observableA.getSnapshot()).toEqual('waw1_2')
+    expect(observableB.getSnapshot()).toEqual('waw2_3')
+    expect(observableC.getSnapshot()).toEqual('waw3_3')
+    expect(subscriberA.value).toEqual('waw1_2')
+    expect(subscriberB.value).toEqual('waw2_2')
+    expect(subscriberC.value).toEqual('waw3_3')
+
+    unsubscribableA.unsubscribe()
+    unsubscribableC.unsubscribe()
+
+    observableMap.merge([
+      ['A', 'waw1_4'],
+      ['B', 'waw2_4'],
+      ['C', 'waw3_4'],
+    ])
+    expect(observableA.getSnapshot()).toEqual('waw1_4')
+    expect(observableB.getSnapshot()).toEqual('waw2_4')
+    expect(observableC.getSnapshot()).toEqual('waw3_4')
+    expect(subscriberA.value).toEqual('waw1_2')
+    expect(subscriberB.value).toEqual('waw2_2')
+    expect(subscriberC.value).toEqual('waw3_3')
+
+    expect(consoleMock.getIndiscriminateAll()).toMatchInlineSnapshot(`[]`)
   })
 })
