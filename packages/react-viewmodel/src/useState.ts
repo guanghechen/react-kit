e5dd61c0b4af62de452b021584b46eab74e0137e
guanghechen <me.guanghechen@gmail.com>
2024-09-19 12:24:53 +0800

    :art:  improve: expose new method to align the react useState

--- packages/react-viewmodel/src/useState.ts
+++ packages/react-viewmodel/src/useState.ts
@@ -1,8 +1,8 @@
 import type { IState } from '@guanghechen/viewmodel'
-import type React from 'react'
 import { useSyncExternalStore } from './useSyncExternalStore'
 
-type ISetState<T> = (patch: (prev: T) => T) => void
+export type ISetState<T> = (patch: (prev: T) => T) => void
+export type IUpdateState<T> = T | ISetState<T>
 
 export function useStateValue<T>(state$: IState<T>): T {
   const { getSnapshot, getServerSnapshot, subscribeStateChange } = state$
@@ -18,4 +18,10 @@ export function useState<T>(state$: IState<T>): [T, ISetState<T>] {
   const { getSnapshot, getServerSnapshot, subscribeStateChange, setState } = state$
   const state = useSyncExternalStore(subscribeStateChange, getSnapshot, getServerSnapshot)
   return [state, setState]
+}
+
+export function useReactState<T>(state$: IState<T>): [T, IUpdateState<T>] {
+  const { getSnapshot, getServerSnapshot, subscribeStateChange, updateState } = state$
+  const state = useSyncExternalStore(subscribeStateChange, getSnapshot, getServerSnapshot)
+  return [state, updateState]
 }
