5debf66acaf8b91584709fe8141e0ff3f821e4bf
guanghechen <me.guanghechen@gmail.com>
2025-09-01 21:35:17 +0800

    improve(useViewModel): support async function

--- packages/react-viewmodel/src/useViewModel.ts
+++ packages/react-viewmodel/src/useViewModel.ts
@@ -1,27 +1,42 @@
 import type { IViewModel } from '@guanghechen/viewmodel'
 import React from 'react'
 
-export const useViewModel = <T extends IViewModel & { name?: string | null }>(
-  fn: () => T,
-): T | null => {
+type V = IViewModel & { name?: string | null }
+type F<T extends V> = () => T | Promise<T>
+
+export const useViewModel = <T extends V>(fn: F<T>): T | null => {
   const ref = React.useRef<T | null>(null)
   const [_, setTick] = React.useState<number>(0)
 
-  const fnRef = React.useRef<() => T>(fn)
+  const fnRef = React.useRef<F<T>>(fn)
   fnRef.current = fn
 
   React.useEffect(() => {
+    let cancelled: boolean = false
     if (!ref.current) {
-      ref.current = fnRef.current()
+      const promise: Promise<T> = Promise.resolve(fnRef.current())
+      void promise
+        .then((viewmodel: T): void => {
+          ref.current = viewmodel
+          const name: string = viewmodel.name || viewmodel.constructor.name
+          console.log(`[useViewModel] creating ${name}.`)
 
-      const viewmodel: T = ref.current
-      const name: string = viewmodel.name || viewmodel.constructor.name
-      console.log(`[useViewModel] creating ${name}.`)
+          if (!cancelled) {
+            setTick(c => c + 1)
+          }
+        })
+        .catch(error => {
+          ref.current = null
+          console.log('[useViewModel] failed. error:', error)
+
+          if (!cancelled) {
+            setTick(c => c + 1)
+          }
+        })
     }
 
-    setTick(c => c + 1)
-
     return () => {
+      cancelled = true
       const viewmodel: T | null = ref.current
       if (!viewmodel) return
 
